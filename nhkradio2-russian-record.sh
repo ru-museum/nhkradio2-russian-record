44a4f2bb04dbab2cec2822646a066cc4b72bf4b3
ru-museum <museum_ru@yahoo.co.jp>
2021-09-13 12:48:41 +0900

    Create nhkradio2-russian-record.sh

---
+++ nhkradio2-russian-record.sh
@@ -0,0 +1,40 @@
+
+#!/bin/bash
+
+TITLE="NHKまいにちロシア語"
+
+# 東京 NHK第二放送
+# 放送波URL: 以下で取得出来ます（2021）。 
+# https://www.nhk.or.jp/radio/config/config_web.xml
+
+# 2021年更新
+M3U8URL="https://radio-stream.nhk.jp/hls/live/2023229/nhkradiruakr1/master.m3u8"
+# 旧URL：上記で動作しない場合試して下さい。
+# M3U8URL="https://nhkradioakr2-i.akamaihd.net/hls/live/511929/1-r2/1-r2-01.m3u8"
+
+# 曜日設定
+arr=("日" "月" "火" "水" "木" "金" "土")
+DAY=`date '+%w'`
+_DAY="(${arr[$DAY]})"
+YEAR=`date '+%Y'`
+DATE1=`date '+%m%d'`
+DATE2=`date '+%H:%M'`
+DATE="${YEAR}${DATE1}${_DAY}-${DATE2}"
+
+# 録音時間 ： 15分
+# REC_TIME="00:15:00" 
+
+# 録音データ保存フォルダ名：絶対PATHを指定
+MY_OUTDIR="/your/directory/name/NHKロシア語講座"
+
+# 録音講座の振り分け：【入門編】【応用編】（番組表参照）
+COURCE=""
+if [ "$DAY" -eq 4 -o "$DAY" -eq 5 ]; then
+  COURCE="【応用編】"
+else
+  COURCE="【入門編】"
+fi
+
+# 録音 ⇒ 保存処理: NHKまいにちロシア語【入門編】-20210913(月)-08:50.m4a
+# M4A: 音声ファイルのみのフォーマットで iTunes などで再生出来ます。
+ffmpeg -i "${M3U8URL}" -t "${REC_TIME}" -c copy "${MY_OUTDIR}/${TITLE}${COURCE}-${DATE}".m4a
