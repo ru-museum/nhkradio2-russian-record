3dc5ff1302c901312e5183159a8df238dc50c1ae
ru-museum <museum_ru@yahoo.co.jp>
2021-09-15 19:25:37 +0900

    Update nhkradio2-russian-record.sh

--- nhkradio2-russian-record.sh
+++ nhkradio2-russian-record.sh
@@ -32,13 +32,13 @@ sleep 0
 MY_OUTDIR="/your/directory/name/NHKロシア語講座"
 
 # 録音講座の振り分け：【入門編】【応用編】（番組表参照）
-COURCE=""
+COURSE=""
 if [ "$DAY" -eq 4 -o "$DAY" -eq 5 ]; then
-  COURCE="【応用編】"
+  COURSE="【応用編】"
 else
-  COURCE="【入門編】"
+  COURSE="【入門編】"
 fi
 
 # 録音 ⇒ 保存処理 ⇒ ファイル名: NHKまいにちロシア語【入門編】-20210913(月)-08:50.m4a
 # M4A: 音声ファイルのみのフォーマットで iTunes などで再生出来ます。
-ffmpeg -i "${M3U8URL}" -t "${REC_TIME}" -c copy "${MY_OUTDIR}/${TITLE}${COURCE}-${DATE}".m4a
+ffmpeg -i "${M3U8URL}" -t "${REC_TIME}" -c copy "${MY_OUTDIR}/${TITLE}${COURSE}-${DATE}".m4a
